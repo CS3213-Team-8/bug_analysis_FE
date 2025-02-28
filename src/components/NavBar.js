1eb31a5242a773f2ea285213271f24475b1fce31
jocelyn-soh <s.sohjoce@gmail.com>
2025-03-01 01:08:08 +0800

    Add greyed out text when page is not selected on navigation bar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,13 +1,41 @@
 import React from 'react';
-import { AppBar, Toolbar, Typography, Button } from '@mui/material';
-import { Link } from 'react-router-dom';
+import { AppBar, Toolbar, Button } from '@mui/material';
+import { Link, useLocation } from 'react-router-dom';
 
 const NavBar = () => {
+  const location = useLocation();
+
   return (
     <AppBar position="static" elevation={0}>
       <Toolbar>
-        <Button color="inherit" component={Link} to="/" sx={{ textTransform: 'none', mr: 3 }}>Visualization</Button>
-        <Button color="inherit" component={Link} to="/bugsList" sx={{ textTransform: 'none' }}>Bugs List</Button>
+        <Button color="inherit" 
+          component={Link} 
+          to="/" 
+          sx={{ 
+            textTransform: 'none', 
+            mr: 3,
+            color: location.pathname === '/' ? 'white' : '#546379',
+            '&:hover': {
+              color: 'rgba(255, 255, 255, 0.8)',
+            },
+          }}
+        >
+          Visualization
+        </Button>
+        <Button
+          color="inherit"
+          component={Link}
+          to="/bugsList"
+          sx={{
+            textTransform: 'none',
+            color: location.pathname === '/bugsList' ? 'white' : '#546379',
+            '&:hover': {
+              color: 'rgba(255, 255, 255, 0.8)',
+            },
+          }}
+        >
+          Bugs List
+        </Button>
       </Toolbar>
     </AppBar>
   );
