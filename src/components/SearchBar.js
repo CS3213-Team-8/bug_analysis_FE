bffced8c26bfe507cd1bbfdb01846b34fbbb8473
AriellaCallista <e0960546@u.nus.edu>
2025-03-02 15:24:15 +0800

    Add search bar with the modified theme

---
+++ src/components/SearchBar.js
@@ -0,0 +1,89 @@
+import React, { useState } from "react";
+import { alpha, styled } from "@mui/material/styles";
+import { InputBase, IconButton, Box } from "@mui/material";
+import SearchIcon from "@mui/icons-material/Search";
+import ClearIcon from "@mui/icons-material/Clear"; 
+
+const SearchBarContainer = styled("div")(({ theme }) => ({
+  borderRadius: "2rem",
+  backgroundColor: alpha(theme.palette.common.white, 0.15),
+  "&:hover": {
+    backgroundColor: alpha(theme.palette.common.white, 0.25),
+  },
+  marginLeft: "2.5rem", // space for search icon
+  width: "100%",
+  display: "flex",
+  alignItems: "center",
+}));
+
+const SearchIconWrapper = styled("div")(() => ({
+  position: "absolute",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+  pointerEvents: "none",
+}));
+
+const StyledInput = styled(InputBase)(() => ({
+  flexGrow: 1, // Ensure the input takes up available space
+  "& .MuiInputBase-input": {
+    padding: "0.7rem 1.5rem",
+    width: "100%",
+  },
+}));
+
+const SearchBar = ({
+  value,
+  onChange,
+  onSearch,
+  placeholder = "Search...",
+  style = {},
+  disabled = false,
+}) => {
+  const [inputValue, setInputValue] = useState(value);
+
+  const handleInputChange = (e) => {
+    setInputValue(e.target.value);
+    onChange(e.target.value); // Propagate the change to parent component
+    onSearch(e.target.value); // Perform search in real-time
+  };
+
+  const handleClear = () => {
+    setInputValue(""); // Clear the input
+    onChange(""); // Notify parent of the cleared input
+  };
+
+  // used to trigger search on key press
+//   const handleSearch = () => {
+//     onSearch(inputValue); // Trigger search logic
+//     console.log("handle search");
+//   };
+
+  return (
+    <Box style={style} justifyContent="center" alignItems="center">
+      <SearchIconWrapper>
+        <SearchIcon sx={{ mt: "0.4rem" , fontSize: "40px", color: "white" }} />
+      </SearchIconWrapper>
+      <SearchBarContainer>
+        <StyledInput
+          value={inputValue}
+          onChange={handleInputChange}
+          placeholder={placeholder}
+          disabled={disabled}
+        //   onKeyDown={(e) => {
+        //     if (e.key === "Enter") {
+        //       handleSearch(); // Trigger search when Enter is pressed
+        //     }
+        //   }}
+        />
+        {inputValue && (
+          <IconButton sx={{ mr: "0.5rem" }} onClick={handleClear}>
+            <ClearIcon htmlColor="#ffff" />
+          </IconButton>
+        )}
+      </SearchBarContainer>
+    </Box>
+  );
+};
+
+export default SearchBar;
--- src/theme.js
+++ src/theme.js
@@ -3,14 +3,52 @@ import { createTheme } from '@mui/material/styles';
 const theme = createTheme({
   palette: {
     primary: {
-      main: '#1976d2', // Blue
+      main: '#4E9D94', // Mint Green 
+      light: '#98D0C9', 
+      dark: "#2f5e59",
+      contrastText: '#FFFF'
     },
     secondary: {
-      main: '#d32f2f', // Red
+      main: '#4F378A', // Purple
     },
+    text: {
+      primary: '#F5F5F5', 
+      secondary: '#BDC9DD', 
+      disabled: '#546379'
+    },
+    background: {
+      default: "#1E2631",
+      light: "#242c37"
+    }
   },
   typography: {
-    fontFamily: 'Arial, sans-serif',
+    fontFamily: ["Inter", "sans-serif"].join(","),
+    fontSize: 16,
+    h1: {
+      fontFamily: ["Inter", "Montserrat", "sans-serif"].join(","),
+      fontSize: 24,
+    },
+    h2: { // for website title i.e. BUGSNAPPER
+      fontFamily: ["Monserrat", "Inter", "sans-serif"].join(","),
+      fontSize: 18,
+      fontWeight: 700
+    },
+    h3: {
+      fontFamily: ["Inter", "sans-serif", "Montserrat"].join(","),
+      fontSize: 18,
+      fontWeight: 700
+    },
+    h4: {
+      fontFamily: ["Inter", "sans-serif"].join(","),
+      fontSize: 16,
+      fontWeight: 500,
+    },
+    subtitle1: { 
+      fontFamily: ["Inter", "sans-serif"].join(","),
+      fontSize: 14,
+      color: "#BDC9DD"
+    },
+    
   },
 });
 
