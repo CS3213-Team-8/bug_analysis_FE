f80163ad21eed56be7a46c4dfd7cd6f9afc74055
AriellaCallista <e0960546@u.nus.edu>
2025-03-02 14:58:39 +0800

    Apply custom theme

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
-import { Container } from '@mui/material';
+import { CssBaseline, Container, ThemeProvider } from '@mui/material';
 import Home from './pages/Home';
 import About from './pages/About';
 import Counter from './pages/Counter';
 import NavBar from './components/NavBar';
+import theme from './theme';
 
 function App() {
   return (
     <Router>
-      <NavBar />
+       <ThemeProvider theme={theme}>
+       <CssBaseline /> 
+        <NavBar />
 
-      <Container sx={{ mt: 4 }}>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/counter" element={<Counter />} />
-        </Routes>
-      </Container>
+        <Container sx={{ mt: 4 }}>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/counter" element={<Counter />} />
+          </Routes>
+        </Container>
+      </ThemeProvider>
     </Router>
+   
   );
 }
 
