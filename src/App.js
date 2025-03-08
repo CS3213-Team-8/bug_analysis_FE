36b74c5ca04411b1f7ad18a30c69fb42e8e62878
STELLA-LYE <stellalye2@gmail.com>
2025-03-08 20:36:06 +0800

    Add category page

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import BugsList from './pages/BugsList';
 import NavBar from './components/NavBar';
 import theme from './theme';
 import Dummy from './pages/Dummy';
+import Category from './pages/Category';
 
 function App() {
   return (
@@ -19,6 +20,7 @@ function App() {
           <Route path="/" element={<Visualization />} />
           <Route path="/bugsList" element={<BugsList />} />
           <Route path="/dummy" element={<Dummy />} />
+          <Route path="/category" element={<Category />} />
         </Routes>
       </Container>
       </ThemeProvider>
