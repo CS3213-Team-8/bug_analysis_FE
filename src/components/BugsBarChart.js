b42fda547b44a5cc4c52e04aad47f26ae9dda725
AriellaCallista <e0960546@u.nus.edu>
2025-03-21 00:15:37 +0800

    Update tooltip

--- src/components/BugsBarChart.js
+++ src/components/BugsBarChart.js
@@ -30,6 +30,37 @@ function CustomizedTick(props) {
   )
 }
 
+const CustomTooltip = ({ active, payload, label }) => {
+  if (active && payload && payload.length) {
+    console.log("payload: ", payload);
+
+    // Filter out unwanted entries (e.g., DuckDB, CockroachDB)
+    const filteredPayload = payload.filter(item => item.name === label);
+
+    return (
+      <div
+        style={{
+          backgroundColor: 'rgb(206, 194, 194)',
+          border: 'solid 1px #000',
+          borderRadius: '8px',
+          color: '#000',
+          fontSize: '18px',
+          fontWeight: '600',
+          padding: '10px',
+        }}
+      >
+        <p><strong>{label}</strong></p>
+        {filteredPayload.map((item, index) => (
+          <p key={index} style={{ color: item.color }}>
+            {`${item.name}: ${item.value}`}
+          </p>
+        ))}
+      </div>
+    );
+  }
+  return null;
+};
+
 const BugsBarChart = ({ data = [], xAxisKey, xLabel, yLabel, chartTitle }) => {
   const classes = useStyles()
   const multiChartColors = {
@@ -79,29 +110,30 @@ const BugsBarChart = ({ data = [], xAxisKey, xLabel, yLabel, chartTitle }) => {
   return (
     <div className={classes.chartContainer}>
       {chartTitle && <div className={classes.chartTitle}>{chartTitle}</div>}
-      <ResponsiveContainer width='100%' height={220}>
-        <BarChart
-          data={filteredData}
-          height='250'
-          margin={{ top: 5 }}
-          {...{
-            overflow: 'visible',
-          }}
-          {...(!isMultiBarChart && { barGap: -1 })}
-        >
+      <ResponsiveContainer width="100%" height={220}>
+      <BarChart
+        data={filteredData}
+        height={250}
+        margin={{ top: 5 }}
+        overflow="visible"
+        {...(isMultiBarChart ? {} : { barGap: -1 })}
+      >
           <CartesianGrid strokeDasharray='3 3' />
           <XAxis dataKey={xAxisKey} tick={<CustomizedTick />} angle={-35} />
           <YAxis />
-          <Tooltip
-            contentStyle={{
-              backgroundColor: 'rgb(206, 194, 194)',
-              border: 'solid 1px #000',
-              borderRadius: '8px',
-              color: '#000',
-              fontSize: '18px',
-              fontWeight: '600',
-            }}
-          />
+          {isMultiBarChart
+          ? <Tooltip
+              contentStyle={{
+                backgroundColor: 'rgb(206, 194, 194)',
+                border: 'solid 1px #000',
+                borderRadius: '8px',
+                color: '#000',
+                fontSize: '18px',
+                fontWeight: '600',
+              }}
+            />
+          : <Tooltip content={<CustomTooltip />} />}
+          
           {isMultiBarChart
             ? dbmsKeys.map((key) => (
                 <Bar
